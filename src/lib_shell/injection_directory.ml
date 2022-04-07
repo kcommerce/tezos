86a6e5729e878b0c7106d721bf7c60d7cd76d7ef
François Thiré <francois.thire@nomadic-labs.com>
2022-04-07 14:09:30 +0200

    Shell/RPC: Allow to inject several operations at once

--- src/lib_shell/injection_directory.ml
+++ src/lib_shell/injection_directory.ml
@@ -45,6 +45,69 @@ let inject_block validator ?force ?chain bytes operations =
       let* _ = block in
       return_unit )
 
+(* While injecting several operations, if one injection failed, we
+   have to report the error. To avoid using recursive errors we do the
+   reporting as follows:
+
+   - We wrap the error into [Injection_operations_error]
+
+   - If injecting the operation [oph] succeeded we use
+   [Injection_operation_succeed_case oph]
+
+   - If injecting the operation [oph] failed with [err], we use
+   [Injection_operation_error_case oph] followed by [err].    
+*)
+type Error_monad.error += Injection_operations_error
+
+type Error_monad.error += Injection_operation_succeed_case of Operation_hash.t
+
+type Error_monad.error += Injection_operation_error_case of Operation_hash.t
+
+let () =
+  let open Data_encoding in
+  register_error_kind
+    `Permanent
+    ~id:"injection_operations_error"
+    ~title:"Injection operations error"
+    ~description:
+      "While injecting several operations at once, one or several injections \
+       failed."
+    ~pp:(fun ppf () ->
+      Format.fprintf
+        ppf
+        "While injecting several operations, one or several injections failed. \
+         Errors are the one below in the trace.")
+    unit
+    (function Injection_operations_error -> Some () | _ -> None)
+    (function () -> Injection_operations_error) ;
+  register_error_kind
+    `Permanent
+    ~id:"injection_operation_succeed"
+    ~title:"Injection operation succeed"
+    ~description:
+      "The injection of this operation succeed among a list of injections \
+       containing at least one error."
+    ~pp:(fun ppf oph ->
+      Format.fprintf ppf "Injection of %a succeeded." Operation_hash.pp oph)
+    (obj1 (req "oph" Operation_hash.encoding))
+    (function Injection_operation_succeed_case oph -> Some oph | _ -> None)
+    (function oph -> Injection_operation_succeed_case oph) ;
+  register_error_kind
+    `Permanent
+    ~id:"injection_operation_error"
+    ~title:"Injection operation error"
+    ~description:
+      "The injection of this operation failed. Error is after in the list"
+    ~pp:(fun ppf oph ->
+      Format.fprintf
+        ppf
+        "Injection of %a failed. Error is next."
+        Operation_hash.pp
+        oph)
+    (obj1 (req "oph" Operation_hash.encoding))
+    (function Injection_operation_error_case oph -> Some oph | _ -> None)
+    (function oph -> Injection_operation_error_case oph)
+
 let inject_operation validator ~force ?chain bytes =
   let open Lwt_result_syntax in
   let*! chain_id = read_chain_id validator chain in
@@ -55,6 +118,34 @@ let inject_operation validator ~force ?chain bytes =
   in
   let hash = Operation_hash.hash_bytes [bytes] in
   Lwt.return (hash, t)
+
+let inject_operations validator ~force ?chain bytes_list =
+  let open Lwt_syntax in
+  let* (hashes, promises) =
+    List.fold_left_s
+      (fun (hashes, promises) bytes ->
+        let* (hash, promise) = inject_operation validator ~force ?chain bytes in
+        return (hash :: hashes, promise :: promises))
+      ([], [])
+      bytes_list
+  in
+  let hashes_array = Array.of_list hashes in
+  let fold_errors (has_failed, result, n) promise_result =
+    let oph = Array.get hashes_array n in
+    match promise_result with
+    | Ok () ->
+        (has_failed, Injection_operation_succeed_case oph :: result, n + 1)
+    | Error trace ->
+        (* The list will be reversed. *)
+        (true, trace @ Injection_operation_error_case oph :: result, n + 1)
+  in
+  let join_results l =
+    let (has_failed, result, _) = List.fold_left fold_errors (false, [], 0) l in
+    if not has_failed then Ok ()
+    else Result_syntax.fail (Injection_operations_error :: List.rev result)
+  in
+  let t = Lwt.map join_results (Lwt.all promises) in
+  return (hashes, t)
 
 let inject_protocol store proto =
   let open Lwt_result_syntax in
@@ -91,6 +182,13 @@ let build_rpc_directory validator =
     let* () = if q#async then return_unit else wait in
     return hash
   in
+  let inject_operations q contents =
+    let*! (hashes, wait) =
+      inject_operations validator ~force:q#force ?chain:q#chain contents
+    in
+    let* () = if q#async then return_unit else wait in
+    return hashes
+  in
   register0 Injection_services.S.block (fun q (raw, operations) ->
       let* (hash, wait) =
         inject_block validator ?chain:q#chain ~force:q#force raw operations
@@ -101,6 +199,7 @@ let build_rpc_directory validator =
   register0
     Injection_services.S.private_operation
     (inject_operation ~force:true) ;
+  register0 Injection_services.S.private_operations inject_operations ;
   register0 Injection_services.S.protocol (fun q protocol ->
       let*! (hash, wait) = inject_protocol state protocol in
       let* () = if q#async then return_unit else wait in
